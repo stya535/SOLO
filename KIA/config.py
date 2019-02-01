fb64a8311dac1f657c58ae1f346a51b89f60d3bd
Genta <35428298+stya535@users.noreply.github.com>
2019-02-01 21:24:53 +0300

    Add files via upload

--- KIA/config.py
+++ KIA/config.py
@@ -31,7 +31,7 @@ class Config(object):
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
     APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'SELFBOT-BY:MAXv10'
+    SYSTEM_NAME = 'SELFBOT-BY:SADISv10'
     SYSTEM_VER  = '8.22.17'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
